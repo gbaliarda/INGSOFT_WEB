b8cbe627155b548d190471f2d514b98d2d86a96e
gbaliarda <gbaliarda@itba.edu.ar>
2021-09-17 19:57:51 -0300

    First steps

---
+++ components/Inicio.js
@@ -0,0 +1,21 @@
+import styles from './styles/Inicio.module.css'
+
+export default function Inicio() {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftSec}>
+        <h1>CryptoViper</h1>
+        <h2>Consigue $CRYV en PancakeSwap <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">aqui!</a></h2>
+        <div className={styles.buttons}>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className={styles.btn}>PvP</a>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className={styles.btn}>PvE</a>
+        </div>
+      </div>
+      <div className={styles.rightSec}>
+       <img src="/logoo.svg" alt="CryptoViper Logo" />
+      </div>
+    </div>
+  )
+
+}
---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import styles from './styles/Navbar.module.css'
+
+export default function Navbar() {
+
+  return (
+    <nav className={styles.nav}>
+      <a href="#">CryptoViper</a>
+      <div className={styles.links}>
+        <a href="#">Inicio</a>
+        <a href="#">WhitePaper</a>
+        <a href="#">Entrar con MetaMask</a>
+      </div>
+    </nav>
+  )
+
+}
